e8af9b9ea7ab7618075ab294f4f7d3971204ea5c
stkener <stkener@gmail.com>
2021-09-05 22:31:27 -0300

    portfolio

---
+++ envio.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_POST['email'])) {
+
+<bold>// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias</bold>
+$email_to = "stkener@gmail.com";
+$email_subject = "Mensaje desde portfolio";
+
+<bold>// Aquí se deberían validar los datos ingresados por el usuario</bold>
+if(!isset($_POST['name']) ||
+!isset($_POST['email']) ||
+!isset($_POST['comments'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['name'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Comentarios: " . $_POST['message'] . "\n\n";
+
+
+<bold>// Ahora se envía el e-mail usando la función mail() de PHP</bold>
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+}
+?>
